8c7c50c4ed6664244c58f5aca85fd595bcbc967a
bruno <bruno.ams@hotmail.com>
2023-07-03 20:26:16 -0300

    instantiation

---
+++ backend/innovatsi/src/main/java/net/schreck/innovatsi/configuration/Instantiation.java
@@ -0,0 +1,32 @@
+package net.schreck.innovatsi.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import net.schreck.innovatsi.domain.Sector;
+import net.schreck.innovatsi.repositories.SectorRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private SectorRepository sectorRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        sectorRepository.deleteAll();
+
+        Sector setor1 = new Sector(null, "A");
+        Sector setor2 = new Sector(null, "B");
+
+        sectorRepository.saveAll(Arrays.asList(setor1, setor2));
+
+
+        //throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
