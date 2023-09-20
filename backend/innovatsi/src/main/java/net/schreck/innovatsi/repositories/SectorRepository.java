89dff82481a6a1e095c3d44f838d27fc59a4469d
brunoastor <bruno.ams@hotmail.com>
2023-09-20 12:41:27 -0300

    newproject

--- backend/innovatsi/src/main/java/net/schreck/innovatsi/repositories/SectorRepository.java
+++
@@ -1,9 +0,0 @@
-package net.schreck.innovatsi.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import net.schreck.innovatsi.domain.Sector;
-
-public interface SectorRepository extends MongoRepository<Sector, String> {
-    
-}
